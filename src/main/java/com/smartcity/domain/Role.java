d2b9e5890fd28503aeba7f957cf720f569912e65
Hudyma Vasyl <vasia08071998@gmail.com>
2019-05-02 23:58:45 +0300

    Fix from all comments

--- src/main/java/com/smartcity/domain/Role.java
+++ src/main/java/com/smartcity/domain/Role.java
@@ -1,5 +1,79 @@
 package com.smartcity.domain;
 
-public class Role {
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+public class Role {
+    private Long id;
+    private String name;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    public Role() {
+    }
+
+    public Role(Long id, String name, LocalDateTime updatedDate, LocalDateTime createdDate) {
+        this.id = id;
+        this.name = name;
+        this.updatedDate = updatedDate;
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return id.equals(role.id) &&
+                name.equals(role.name) &&
+                createdDate.equals(role.createdDate) &&
+                Objects.equals(updatedDate, role.updatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, createdDate, updatedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                '}';
+    }
 }
--- src/main/java/com/smartcity/mapper/RoleMapper.java
+++ src/main/java/com/smartcity/mapper/RoleMapper.java
@@ -1,13 +1,32 @@
 package com.smartcity.mapper;
 
 import com.smartcity.domain.Role;
+import com.smartcity.exceptions.NotFoundException;
 import org.springframework.jdbc.core.RowMapper;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class RoleMapper implements RowMapper<Role> {
 
+    private static final RoleMapper instance = new RoleMapper();
+
+    private RoleMapper() {
+    }
+
+    public static RoleMapper getInstance() {
+        return instance;
+    }
+
     public Role mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+        if (resultSet != null) {
+            Role role = new Role();
+            role.setId(resultSet.getLong("id"));
+            role.setName(resultSet.getString("name"));
+            role.setCreatedDate(resultSet.getObject("created_date", LocalDateTime.class));
+            role.setUpdatedDate(resultSet.getObject("updated_date", LocalDateTime.class));
+            return role;
+        } else throw new NotFoundException("Can't get role. ResultSet is null!");
     }
 }
