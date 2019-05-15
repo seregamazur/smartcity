40d10b0dcaac7c0eb1c17da945c5d8901753dd1b
mperehinets <mpereginets1@gmail.com>
2019-05-15 11:52:31 +0300

    Implement OrganizationDAOImpl for Organization entity + TEST

--- src/main/java/com/smartcity/domain/Organization.java
+++ src/main/java/com/smartcity/domain/Organization.java
@@ -1,5 +1,92 @@
 package com.smartcity.domain;
 
-public class Organization {
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+public class Organization {
+    private Long id;
+    private String name;
+    private String address;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    public Organization() {
+
+    }
+
+    public Organization(Long id, String name, String address, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(address, that.address) &&
+                Objects.equals(createdDate, that.createdDate) &&
+                Objects.equals(updatedDate, that.updatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, createdDate, updatedDate);
+    }
 }
