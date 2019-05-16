020215a7e0d3515177e91b0c02d7678fba22d844
AntonKalaturnyi <kalaturny@gmail.com>
2019-05-17 01:03:40 +0300

    updated RegistrationController

--- src/main/java/com/smartcity/controller/RegistrationController.java
+++ src/main/java/com/smartcity/controller/RegistrationController.java
@@ -4,19 +4,20 @@ import com.smartcity.dto.UserDto;
 import com.smartcity.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/registration")
 public class RegistrationController {
 
     @Autowired
     private UserServiceImpl userService;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/registration")
+    @PostMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public UserDto registerUser(@RequestBody UserDto user) {
 
         user.setActive(true);
