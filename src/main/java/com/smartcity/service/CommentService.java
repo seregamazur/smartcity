3b2c05e5dc7b364c0fc2bd9437495353eeddf41c
Bohdan Hrytsak <47381599+BhHaipls@users.noreply.github.com>
2019-05-17 01:39:44 +0300

    ImpCommentController (#44)

--- src/main/java/com/smartcity/service/CommentService.java
+++ src/main/java/com/smartcity/service/CommentService.java
@@ -1,8 +1,23 @@
 package com.smartcity.service;
 
+import com.smartcity.dto.CommentDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface CommentService {
+
+    CommentDto create(CommentDto commentDto);
+
+    CommentDto findById(Long id);
+
+    CommentDto update(CommentDto commentDto);
+
+    boolean delete(Long id);
+
+    List<CommentDto> findAllByTaskId(Long id);
+
+    List<CommentDto> findAllByUserId(Long id);
 
 }
