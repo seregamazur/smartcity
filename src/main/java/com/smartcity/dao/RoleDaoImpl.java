7222795b95f386b89d7ec6aa5e026affce888bef
Vasyl Hudyma <vasia08071998@gmail.com>
2019-05-21 17:18:47 +0300

    Role dao (#57)
    * Added getAll() method
    
    * Created test to getAll() method
    
    * Fixed test to getAll() method
    
    * added method getRolesByUserId()

--- src/main/java/com/smartcity/dao/RoleDaoImpl.java
+++ src/main/java/com/smartcity/dao/RoleDaoImpl.java
@@ -11,7 +11,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
-
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
@@ -67,6 +66,16 @@ public class RoleDaoImpl implements RoleDao {
         }
     }
 
+    public List<Role> getRolesByUserId(Long id){
+        try {
+            return this.jdbcTemplate.query(Queries.SQL_GET_ROLES_BY_USER_ID, RoleMapper.getInstance(), id);
+        }
+        catch (Exception e){
+            logger.error("Can't get Roles by User_id = {}. Error: ",id, e);
+            throw new DbOperationException("Can't get roles by Users_id = id "+id);
+        }
+    }
+
     public List<Role> getAll() {
         try {
             return this.jdbcTemplate.query(Queries.SQL_ROLE_GET_ALL, RoleMapper.getInstance());
@@ -101,6 +110,8 @@ public class RoleDaoImpl implements RoleDao {
         }
     }
 
+
+
     public boolean delete(Long id) {
 
         try {
@@ -124,6 +135,8 @@ public class RoleDaoImpl implements RoleDao {
         return notFoundException;
     }
 
+
+
     class Queries {
 
         static final String SQL_ROLE_DELETE = "delete from    Roles where id = ?";
@@ -135,5 +148,8 @@ public class RoleDaoImpl implements RoleDao {
         static final String SQL_ROLE_GET_BY_ID = "select * from Roles where id = ?";
 
         static final String SQL_ROLE_GET_ALL = "select * from Roles";
+
+        static final String SQL_GET_ROLES_BY_USER_ID = "SELECT * FROM Roles JOIN Users_roles ON Roles.id = Users_roles.role_id WHERE Users_roles.user_id = ?";
+
     }
 }
